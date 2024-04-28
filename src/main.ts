84f94e85c32def90b97145ee5deb933e6e6ff282
mac <mac@macs-MacBook-Air.local>
2024-04-28 15:44:27 +0100

    updated port to 80

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(443);
+  await app.listen(80);
+  console.log("Quickreels-Development-App Listening on port 80...");
+  
 }
 bootstrap();
