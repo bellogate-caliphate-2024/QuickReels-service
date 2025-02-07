9d712840d89040dacbb2f17cb4479cdbc81a34e9
Gavin <longgaving@gmail.com>
2025-02-07 23:36:51 +0100

    create post endpoint

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,16 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
+  HttpCode,
+  Param,
+  Delete,
+  Body,
+  Get,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreatePostDto } from '../posts.dto';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
   export class PostsController {
-    constructor(private readonly postService: PostsService) {
-
-
-
-
-        
-    }
+    constructor(private readonly postService: PostsService) {   }
   
-   
+    @Post('create_post')
+    @HttpCode(201) 
+    @UseInterceptors(FileInterceptor('video')) 
+    async createPost(
+      @UploadedFile() videoFile: Express.Multer.File,
+      @Query() createPostDto: CreatePostDto,
+    ) {
+      if (!videoFile) {
+        throw new BadRequestException('No video file uploaded');
+      }
+        return this.postService.createPost(videoFile, createPostDto);
+    }
 
   }
