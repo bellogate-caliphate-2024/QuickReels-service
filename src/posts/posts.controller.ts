ef4b0439c0daf1205ab3a00ed3e5306f096b78f7
Gavin <longgaving@gmail.com>
2025-02-07 18:59:34 +0100

    y

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+  export class PostsController {
+    constructor(private readonly postService: PostsService) {}
+  
+
+    @Get('/hello')
+    async mockdata(){
+      return await this.postService.mock();
+    }
+  }
