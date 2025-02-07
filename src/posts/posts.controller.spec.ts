791dfd7761bbe412fa8d07d4982e5290e34a6622
Gavin <longgaving@gmail.com>
2025-02-08 00:19:26 +0100

    create post endpoint

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,3 +1,5 @@
+jest.mock('firebase-admin'); // Add this at the very top
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
