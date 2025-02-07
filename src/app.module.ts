f66e2a4c5baa254293d29feb7ac346209f2c20bf
Gavin <longgaving@gmail.com>
2025-02-07 19:21:45 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostModule } from './nest/post/post.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostModule, PostsModule],
+  imports: [PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 describe('PostsController', () => {
   let controller: PostsController;
+  let postsService: PostsService;
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,6 +13,8 @@ describe('PostsController', () => {
     }).compile();
 
     controller = module.get<PostsController>(PostsController);
+    postsService = module.get<PostsService>(PostsService);
+
   });
 
   it('should be defined', () => {
