070474a4ce2cca0c3deeec0de871ad33b9d0bc81
Gavin <longgaving@gmail.com>
2025-02-08 01:16:44 +0100

    updated .spec files

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,31 +1,21 @@
 // posts.service.spec.ts
-jest.mock('firebase-admin');
-
-import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
-import * as admin from 'firebase-admin';
 import { mockFile } from '../__mock__/file'; 
 import { CreatePostDto } from '../posts.dto'; 
-
-// Mock Firestore
-const mockFirestore = {
-  collection: jest.fn().mockReturnThis(),
-  doc: jest.fn().mockReturnThis(),
-  set: jest.fn().mockResolvedValue({}),
-};
-
-(admin.firestore as unknown as jest.Mock) = jest.fn(() => mockFirestore);
-(admin.credential.cert as jest.Mock) = jest.fn(() => ({}));
 
 describe('PostsService', () => {
   let service: PostsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
-    }).compile();
+  
+  const mockPostModel = {
+    create: jest.fn().mockResolvedValue({}),
+  };
+  const mockHelper = {
+    someHelperMethod: jest.fn(),
+  };
 
-    service = module.get<PostsService>(PostsService);
+  beforeEach(() => {
+    service = new PostsService(mockPostModel as any, mockHelper as any);
   });
 
   it('should create a post and return a file and a message', async () => {
@@ -38,13 +28,12 @@ describe('PostsService', () => {
     };
     const mockMessage = 'Post created successfully';
 
-    // Mock the service method to return the mock file and a message
     jest.spyOn(service, 'createPost').mockResolvedValue({
       file: mockFile,
       message: mockMessage,
     });
 
-    // Pass the mock file and post data to the service method
+    // Call the method
     const result = await service.createPost(mockFile, mockPost);
 
     // Test the return value
