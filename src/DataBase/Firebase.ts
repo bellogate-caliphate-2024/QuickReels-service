ac3ee1d5358ac1c0156633a0fc0c69fccef7e17e
Gavin <longgaving@gmail.com>
2025-02-10 21:14:15 +0100

    updated createpost function

--- src/DataBase/Firebase.ts
+++ src/DataBase/Firebase.ts
@@ -1,17 +1,19 @@
 import * as admin from 'firebase-admin';
 import { config } from 'dotenv';
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 config();
 
-
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.quickReels_ID,
-      clientEmail: process.env.quickReels_EMAIL,
-      privateKey: (process.env.quickReels_PRIVATE_KEY || '').replace(/\\n/g, '\n')
-    }),
-    storageBucket: 'quickreels-service.appspot.com', 
-  });
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.quickReels_ID,
+    clientEmail: process.env.quickReels_EMAIL,
+    privateKey: (process.env.quickReels_PRIVATE_KEY || '').replace(
+      /\\n/g,
+      '\n',
+    ),
+  }),
+  storageBucket: 'quickreels-service.appspot.com',
+});
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -26,6 +28,3 @@ const firebaseConfig = {
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAdmin = admin;
-
-
-
