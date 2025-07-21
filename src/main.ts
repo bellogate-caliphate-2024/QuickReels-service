935f8b084b2c7617acb9ca0e1926945982b7ce9e
SweetGavo <longgaving@gmail.com>
2025-07-21 15:04:32 +0100

    feat: implement load balancing using node cluster

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,49 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { CustomLogger } from './app/logger/logger.service';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cluster from 'cluster';
+import { cpus } from 'os';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: new CustomLogger(),
+const numCPUs = cpus().length;
+
+if ((cluster as any).isPrimary) {
+  console.log(`Primary server ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    (cluster as any).fork();
+  }
+
+  (cluster as any).on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log('Forking a new worker');
+    (cluster as any).fork();
   });
+} else {
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule, {
+      logger: new CustomLogger(),
+    });
 
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  });
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    });
 
-  
-  // Swagger configuration
-  const config = new DocumentBuilder()
-    .setTitle('QuickReels API')
-    .setDescription('API documentation for QuickReels')
-    .setVersion('1.0')
-    .addBearerAuth() // For Authorization: Bearer <token>
-    .build();
+    
+    // Swagger configuration
+    const config = new DocumentBuilder()
+      .setTitle('QuickReels API')
+      .setDescription('API documentation for QuickReels')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document); // Accessible at http://localhost:3001/api/docs
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3001);
-  console.log('Server is running on port', process.env.PORT ?? 3001);
+    await app.listen(process.env.PORT ?? 3001);
+    console.log('Server is running on port', process.env.PORT ?? 3001);
+  }
+  bootstrap();
 }
-bootstrap();
